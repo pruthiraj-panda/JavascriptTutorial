800b9e7c71159d648bda9d2b58aae1eb92397fab
pruthiraj-panda <123632548+pruthiraj-panda@users.noreply.github.com>
2023-11-21 19:14:41 +0000

    data type

---
+++ .devcontainer/Basicjs/datatype.js
@@ -0,0 +1,17 @@
+//primite 
+// 7 type string ,number,Boolean ,null undefined ,Symbol,Big int
+
+// non primive or refernece
+//Arrays,objects,Functions
+const score=100
+const isLoggesdIn=true;
+let userEmail=undefined
+
+const id=Symbol('123')
+const anotherid=Symbol('123')
+console.log(id==anotherid)
+// type of data cheak
+console.log(typeof 6)
+console.log(typeof null)
+let arr=["pruthi","java","javascript"]
+console.log(typeof id)
