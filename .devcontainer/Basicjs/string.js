51df688571dfac72d5c0fc549bc15bbd9dbd111f
pruthiraj-panda <123632548+pruthiraj-panda@users.noreply.github.com>
2023-11-21 19:46:33 +0000

    string

---
+++ .devcontainer/Basicjs/string.js
@@ -0,0 +1,20 @@
+const name="pruthiraj";
+const sde="google"
+let x=`${name} sde at ${sde}  `;
+console.log(x)
+const gameName=new String("javascript")
+console.log(gameName.length)
+let str=gameName.toUpperCase()
+console.log(str)
+//console.log(gameName)
+console.log(gameName.charAt(5))
+console.log(gameName.indexOf("s"))
+let s=gameName.substring(-3,5) // start from zero index
+let t=gameName.slice(2,5)
+console.log(s)
+let s2="   java  "
+console.log(s2)
+// let g=s2.trim()
+console.log(s2.replace(' ','__'))
+console.log(s2.includes('java'))
+//console.log(g)
