cf78d942e32df1bbe6fe6f24c9866895c1b50c9c
pruthiraj-panda <123632548+pruthiraj-panda@users.noreply.github.com>
2023-12-02 06:58:35 +0000

    switch statement

---
+++ .devcontainer/Basicjs/Switchstatement.js
@@ -0,0 +1,15 @@
+const month=3
+switch (month) {
+    case 1:
+        console.log("january")
+        break;
+    case 2:
+        console.log("february")
+        break;
+    case 3:
+        console.log("march")
+        break;
+    default:
+        console.log("not match")
+        break;
+}
