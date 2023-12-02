e659ed58f6bd21801f80bbd041f980b7de8186f6
pruthiraj-panda <123632548+pruthiraj-panda@users.noreply.github.com>
2023-12-02 06:57:25 +0000

    conditional 2

---
+++ .devcontainer/Basicjs/Conditional.js
@@ -0,0 +1,33 @@
+// if statement
+const isUserlogedin=true
+// if(isUserlogedin){
+//     console.log("yes user loged in")
+// }
+// if(2!=='2'){
+//     console.log("exicuted")
+// }else{
+//     console.log('not exicuted')
+// }
+
+// const score=100
+// if(score>50){
+//     const power='fly'
+//     console.log(power)
+//}
+
+const balance=600
+// if(balance>400) console.log("yes"),console.log("no")
+
+// const userlogedin=true;
+// const debitcard=true;
+// if(userlogedin&&debitcard){
+//     console.log("verified")
+// }else{
+//     console.log("please logged in your account")
+// }
+
+const loggedingoogle=true
+const loggedinfacebook=false
+if(loggedingoogle||loggedinfacebook){
+    console.log("verified")
+}
