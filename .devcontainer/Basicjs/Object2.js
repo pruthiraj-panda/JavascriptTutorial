841930f34703c38a5d3dadb7151f70bc454b5886
pruthiraj-panda <123632548+pruthiraj-panda@users.noreply.github.com>
2023-12-02 06:56:18 +0000

    object part 2

---
+++ .devcontainer/Basicjs/Object2.js
@@ -0,0 +1,18 @@
+// object part 2
+// Destruchering
+
+const course={
+    nameStudent:"pruthiraj",
+    id:"B122085"
+}
+
+const {nameStudent: name}=course // nameStudent == name
+console.log(name)
+
+{
+
+}
+[
+    {},
+    {}
+] 
